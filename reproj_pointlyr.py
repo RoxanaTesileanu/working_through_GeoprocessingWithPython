445c09f5c05ffd6db442bc7b50b7d2b5ceba3655
Roxana <roxana.te@web.de>
2016-09-06 15:27:34 +0300

    [created a new SR for the point layer]

--- reproj_pointlyr.py
+++ reproj_pointlyr.py
@@ -19,3 +19,8 @@ GEOGCS["GCS_WGS_1984",
     UNIT["Degree",0.017453292519943295]]
 >>> # I want to reproject the point layer to another SRS
 >>> 
+>>> from osgeo import osr
+>>> newsr = osr.SpatialReference()
+>>> newsr.ImportFromProj4(''' +title=Stereo 70 +proj=sterea +lat_0=46 +lon_0=25 +k=0.99975 +x_0=500000 +y_0=500000 +ellps=krass +towgs84=33.4,-146.6,-76.3,-0.359,-0.053,0.844,-0.84 +units=m +no_defs''')
+0
+>>> 
