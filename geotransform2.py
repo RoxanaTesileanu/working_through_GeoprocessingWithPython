eea07c38a34117401923f3127b130d31d16a1aad
Roxana <roxana.te@web.de>
2016-09-12 15:03:38 +0300

    [extracting a subset]

--- geotransform2.py
+++ geotransform2.py
@@ -60,4 +60,76 @@ TypeError: ApplyGeoTransform() takes exactly 3 arguments (4 given)
 >>> value = band.ReadAsArray(xoff, yoff, 1, 1) [0,0]
 >>> print value
 1030
+>>> import numpy as np
+>>> # for more than one values :
+>>> 
+>>> data = band.ReadAsArray()
+>>> x,y = map(int, gdal.ApplyGeoTransform(inv_gt, 25.5, 45.5)
+	  )
+>>> print x,y
+1800 1800
+>>> value = data[y,x]
+>>> print value
+1030
+>>> # I want to extract a subset with the upper left coordinates 45.5, 25.5 and lower-right coordinates 45.00, 25.00.
+>>> 
+>>> ulx, uly = 45.5, 25.5
+>>> lrx, lry = 45.00, 25.00
+>>> offsets_ul = gdal.ApplyGeoTransform(inv_gt, ulx, uly)
+>>> offsets_lr = gdal.ApplyGeoTransform(inv_gt, lrx, lry)
+>>> print offsets_ul, offsets_lr
+[73800.5, 73800.5] [72000.5, 75600.5]
+>>> off_ulx, off_uly = map(int, offsets_ul)
+>>> off_lrx, off_lry = map(int, offsets_lr)
+>>> print off_ulx, off_uly, off_lrx, off_lry)
+SyntaxError: invalid syntax
+>>> print off_ulx, off_uly, off_lrx, off_lry
+73800 73800 72000 75600
+>>> # ok I've discovered I acutally have a upper right point and a lower left point.
+>>> ulx, uly = 25.5, 45.5
+>>> lrx, lry = 25.00, 45.00
+>>> offsets_ul = gdal.ApplyGeoTransform(inv_gt, ulx, uly)
+>>> offsets_lr = gdal.ApplyGeoTransform(inv_gt, lrx, lry)
+>>> print offsets_ul, offsets_lr
+[1800.5, 1800.5] [0.5, 3600.5]
+>>> off_ulx, off_uly = map(int, offsets_ul)
+>>> off_lrx, off_lry = map(int, offsets_lr)
+>>> print off_ulx, off_uly, off_lrx, off_lry
+1800 1800 0 3600
+>>> # I've recomputed x and y  coord. of offsets because I've mixed x with y.
+>>> # now I can find out the no. of rows and of columns to extract to get the subset I wanted:
+>>> rows = off_uly - off_lry
+>>> print rows
+-1800
+>>> rows = off_lry - off_uly
+>>> print rows
+1800
+>>> off_lrx - off_ulx
+-1800
+>>> columns = off_lrx - off_ulx
+>>> print columns
+-1800
+>>> gtiff_driver = gdal.GetDriverByName('GTiff')
+>>> columns = off_ulx - off_lrx
+>>> print columns
+1800
+>>> new_ds = gtiff_driver('mysubset.tif', columns, rows, 3)
+
+Traceback (most recent call last):
+  File "<pyshell#65>", line 1, in <module>
+    new_ds = gtiff_driver('mysubset.tif', columns, rows, 3)
+TypeError: 'Driver' object is not callable
+>>> new_ds = gtiff_driver('mysubset.tif', columns, rows)
+
+Traceback (most recent call last):
+  File "<pyshell#66>", line 1, in <module>
+    new_ds = gtiff_driver('mysubset.tif', columns, rows)
+TypeError: 'Driver' object is not callable
+>>> new_ds = gtiff_driver('mysubset.tif', columns, rows, bands=1)
+
+Traceback (most recent call last):
+  File "<pyshell#67>", line 1, in <module>
+    new_ds = gtiff_driver('mysubset.tif', columns, rows, bands=1)
+TypeError: 'Driver' object is not callable
+>>> new_ds = gtiff_driver.Create('mysubset.tif', 1800, 1800, bands=1)
 >>> 
